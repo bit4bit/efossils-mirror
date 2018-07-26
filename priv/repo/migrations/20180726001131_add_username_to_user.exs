e25598e81472b3e1274b5eb529a20af0a3deec80
bit4bit <bit4bit@noemail.net>
2018-07-26 00:38:55 +0000

    archivo de migración para nuevo atributo *username*
    FossilOrigin-Name: 3e33f36608bdc6023269bac87b2e265e11b91ced4bb3ae39367bb77a515e1a07

---
+++ priv/repo/migrations/20180726001131_add_username_to_user.exs
@@ -0,0 +1,10 @@
+defmodule Efossils.Repo.Migrations.AddUsernameToUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :username, :string
+    end
+    create unique_index(:users, [:username])
+  end
+end
